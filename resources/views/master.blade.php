748acc4c0a41a06892257006b1e0bd4bda069308
Angelo Augusto <angelo.costa@ef5.com.br>
2024-09-02 20:49:28 -0300

    Adicionado style e adicionado master paage

---
+++ resources/views/master.blade.php
@@ -0,0 +1,181 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="robots" content="index, follow, all">
+<meta name="googlebot" content="index, follow, all">
+<meta name="google" content="notranslate">
+<meta name="author" content="incatel.com.br">
+
+<script src="js/jquery/jquery.min.js"></script>
+
+<link href="css/bootstrap/bootstrap.css" rel="stylesheet">
+<link href="css/estilo/estilo.css" rel="stylesheet">
+<link href="css/responsivo/responsivo.css" rel="stylesheet">
+
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<div class="topo">
+    <!-- RECIPIENTE -->
+    <div class="container">
+        <!-- MENU RESPONSIVO -->
+        <div class="menu-responsivo">
+            <div class="navbar" role="navigation">
+                <!-- LOGO -->
+                <div class="logo-responsivo">
+                    <a href="">
+                        <img class="img-responsive" src="img/logo-transportadora-visao.png">
+                    </a>
+                </div>
+                <!-- /LOGO -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Trocar Navegação</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="">Início</a></li>
+                        <li><a href="a-empresa">Empresa</a></li>
+                        <li><a href="sassmaq">Sassmaq</a></li>
+                        <li><a href="servicos">Serviços</a></li>
+                        <li><a href="frota">Frota</a></li>
+                        <li><a href="clientes">Clientes</a></li>
+                        <li><a href="cotacao">Cotação</a></li>
+                        <li><a href="contato">Fale Conosco</a></li>
+                        <li><a href="localizacao">Localização</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /MENU RESPONSIVO -->
+
+        <!-- MENU INSTITUCIONAL -->
+        <div class="menu-institucional">
+            <ul>
+                <li>Endereço: Praça da República, 62 conj 144 Centro - Santos - SP CEP: 11013-921</li>
+                <li class=""><i class="glyphicon glyphicon-earphone"></i> <a href="contato">Fale Conosco</a></li>
+                <li class=""><i class="glyphicon glyphicon-map-marker"></i> <a href="localizacao">Localização</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /MENU INSTITUCIONAL -->
+
+        <!-- LOGO -->
+        <div class="logo">
+            <a href=""><img class="img-responsive" src="img/logo-transportadora-visao.png"></a>
+            <img class="img-responsive" src="img/sassmaq-empresa-aprovada-transporte-rodoviario.png">
+        </div>
+        <!-- /LOGO -->
+
+        <!-- MENU DE NAVEGAÇÃO -->
+        <div class="menu-navegacao">
+            <ul>
+                <li class="inicio">
+                    <a href="">Início</a>
+                </li>
+
+                <li class="a-empresa">
+                    <a href="a-empresa">A Empresa</a>
+                </li>
+
+                <li class="sassmaq') 'ativo' : ''; ?>">
+                    <a href="sassmaq">SASSMAQ</a>
+                </li>
+
+                <li class="servicos">
+                    <a href="servicos">Serviços</a>
+                </li>
+
+                <li class="frota">
+                    <a href="frota">Frota</a>
+                </li>
+
+                <li class="clientes">
+                    <a href="clientes">Clientes</a>
+                </li>
+
+                <li class="cotacao">
+                    <a href="cotacao">Cotação</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /MENU DE NAVEGAÇÃO -->
+
+        <!-- BANNER -->
+
+
+        <!-- /BANNER -->
+
+    </div>
+    <!-- RECIPIENTE -->
+</div>
+
+{{-- Roda pé --}}
+<div class="rodape">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-sm-5 col-xs-12">
+                <div class="rodape-dados-de-contato">
+                    <div class="titulo">
+                        <h4>Dados de Contato</h4>
+                    </div>
+                    <ul>
+                        <li><i class="glyphicon glyphicon-earphone"></i> Telefone
+                            <h5>+55 (13) 3216-1910</h5>
+                            <h5>+55 (13) 3219-1033</h5>
+                            <h5>+55 (13) 3219-1699</h5>
+                        </li>
+                        <li><i class="glyphicon glyphicon-envelope"></i> E-mail
+                            <h5>falecom@transportadoravisao.com.br</h5>
+                        </li>
+                        <li><i class="glyphicon glyphicon-map-marker"></i> Endereço
+                            <h5>Endereço: Praça da República, 62 conj 144 Centro - Santos - SP CEP: 11013-921</h5>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3 col-xs-12">
+                <div class="rodape-mapa-do-site">
+                    <div class="titulo">
+                        <h4>Mapa do site</h4>
+                    </div>
+                    <ul>
+                        <li><a href="">Início</a></li>
+                        <li><a href="a-empresa">A Empresa</a></li>
+                        <li><a href="sassmaq">SASSMAQ</a></li>
+                        <li><a href="servicos">Serviços</a></li>
+                        <li><a href="frota">Frota</a></li>
+                        <li><a href="clientes">Clientes</a></li>
+                        <li><a href="cotacao">Cotação</a></li>
+                        <li><a href="contato">Fale Conosco</a></li>
+                        <li><a href="localizacao">Localização</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <div class="rodape-links-uteis">
+                    <div class="titulo">
+                        <h4>Links Úteis</h4>
+                    </div>
+                    <ul>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <li><a href="" target="_blank"></a></li>
+                            </div>
+                            <div class="col-md-6">
+                                <li><a href="" target="_blank"></a></li>
+                                <li><a href="links-uteis">Veja todos...</a></li>
+                            </div>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
